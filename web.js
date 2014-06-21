45f86c2e687d2eed9ce8a94698a907034d9c54b7
Sitao Xie <xiesita@kapai.ecs.vuw.ac.nz>
2014-06-21 19:52:52 +1200

    asdf

--- web.js
+++ web.js
@@ -17,21 +17,7 @@ app.get('/', function(req, res) {
     if (!result) {
       return res.send('No data found');
     } else {
-      res.send('Event name: ' + result.eventname + "<br></br>" + 'date: ' + result.date+ "<br></br>");
-    }
-  });
-});
-
-app.get('/', function(req, res) {
-    
-  query = client.query('SELECT * FROM events where eventid=2');
-  query.on('row', function(result1) {
-    console.log(result1);
-
-    if (!result1) {
-      return res.send('No data found');
-    } else {
-      res.send('Event name: ' + result1.eventname + "<br></br>" + 'date: ' + result1.date);
+      res.send('Event name: ' + result);
     }
   });
 });
