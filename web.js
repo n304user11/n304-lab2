c61dd109c3d698df2e2478d1d33bda30377b6596
Sitao Xie <xiesita@kapai.ecs.vuw.ac.nz>
2014-06-21 20:16:55 +1200

    ijkl

--- web.js
+++ web.js
@@ -17,7 +17,7 @@ app.get('/', function(req, res) {
     if (!result) {
       return res.send('No data found');
     } else {
-      reply = reply + 'Event name: ' + result.eventname + '\n';
+      reply = reply + 'Event name: ' + result.eventname + '<br>';
     }
   });
   query.on('end', function() {
