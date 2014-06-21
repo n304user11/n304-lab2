771ddc0d035c12db4b2af9c210438421cf0b692a
Sitao Xie <xiesita@kapai.ecs.vuw.ac.nz>
2014-06-21 19:13:06 +1200

    asdf

--- web.js
+++ web.js
@@ -9,14 +9,7 @@ client = new pg.Client(connectionString);
 client.connect();
 
 app.get('/', function(req, res) {
-  
-  var a = 'abc';
-  var b = '01/01/2000';
-  var c = 'a,b,c';
-  var d = 'test';
-
-  client.query('INSERT INTO events  (EventName) VALUES(\'abc\')');
-  
+    
   query = client.query('SELECT * FROM events');
   query.on('row', function(result) {
     console.log(result);
@@ -25,6 +18,8 @@ app.get('/', function(req, res) {
       return res.send('No data found');
     } else {
       res.send('Event name: ' + result.EventName);
+      res.send('Event name: ' + result.Date);
+      res.send('Event name: ' + result.People);
     }
   });
 });
